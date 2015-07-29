738d45fc6d0a7158b30d590702f2eb7d8435b242
Jesse Davda <stratosphere1243@gmail.com>
2015-07-29 18:52:08 +0100

    stuff

--- jquery.js
+++ jquery.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
     $('.slider').slider({full_width: true, indicators: false, height: 700});
     $('.parallax').parallax();
+    $('nav').pushpin({ top: $('nav').offset().top });
 });
